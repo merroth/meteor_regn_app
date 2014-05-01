561b1beb1ed53518519e48419fd4dcb30940b4d4
merroth <sj.emil@gmail.com>
2014-05-01 20:39:48 +0200

    Update super_meteor_class.js

--- js/super_meteor_class.js
+++ js/super_meteor_class.js
@@ -1,25 +1,49 @@
 var super_meteor = function()
 {
-	var content = meteor();															//Connect to instance Class
-	content.size = 100;
+	var content = meteor();															//Henter data fra parent class
+	content.type = "super_meteor";													//Ny type beskrivelse
+	content.size = 100;																//Herfra modificeres eller tilføjes værdier
 	content.y -= 70;
 	content.ySpeed += 5;
 	content.decay = function (i)
 	{
 		content.size *= 0.99;
-		if (this.size < 13) {
-		meteorarray.splice(i, 1);
+		if (this.size < 13)
+		{
+			meteorarray.splice(i, 1);
+		}
 	}
 	content.gravity = function ()
 	{
 		content.ySpeed += 0.3;
 		content.ySpeed *= 0.98;
+	},
+	content.explosion = function ()
+	{
+		asteroid.size*=0.8;
+		for(var expl = 0; expl < 50; expl++)
+		{
+			meteorarray.push(new shrapnel_meteor(asteroid.x,asteroid.y,asteroid.size));
+		}
+	},
+	content.boundaries = function (i)
+	{
+		if (asteroid.x < asteroid.size && asteroid.xSpeed < 0 || asteroid.x > framex - asteroid.size && asteroid.xSpeed > 0) {
+			asteroid.xSpeed *= (0 - 1);
+			asteroid.xSpeed *= 0.8;
+			asteroid.explosion();
+		}
+		if (asteroid.y > framey - asteroid.size) {
+			asteroid.ySpeed *= (0 - 1);
+			asteroid.ySpeed *= 0.8;
+			asteroid.explosion();
+		}
 	}
-}
 	
-	return (content);
+	return (content);																//Resultat returneres så det kan instantieres eller videredefineres
 }
 
-if (test) {
+if (test)
+{
 	console.log("super_meteor_class.js");
 }
